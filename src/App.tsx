f49cb90b63e570ccee3d5fb6c0f6ccde64216445
alicek0 <alicekangisy@gmail.com>
2025-01-10 20:41:37 +0900

    fix: resolve merge conflicts in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './index.css'
-
-import { UserProvider } from "./context/UserContext";  // Import the UserProvider
+import "./index.css";
 
 import PreLoginPage from "./pages/PreLoginPage";
 
 /* Nurse Pages */
 import NurseLoginPage from "./pages/nurse/NurseLoginPage";
 import NurseMainPage from "./pages/nurse/NurseMainPage";
+import NurseSchedulePage from "./pages/nurse/NurseSchedulePage";
 
 /* Patient Pages */
 import PatientLoginPage from "./pages/patient/PatientLoginPage";
@@ -17,51 +16,60 @@ import ChoosePatientType from "./pages/patient/ChoosePatientType";
 import PatientChatPage from "./pages/patient/PatientChatPage";
 import PatientChatCategories from "./pages/patient/PatientChatCategoriesPage";
 import CustomRequestPage from "./pages/patient/CustomRequestPage";
+import PatientSchedular from "./pages/patient/PatientSchedular";
+import ScheduleToday from "./pages/patient/ScheduleToday";
 
 /* Context */
 import { FavoriteRequestsProvider } from "./context/FavoriteRequestsContext";
-
-import HospitalInfoPage from './components/HospitalInfoPage';
+import SignUp from "./pages/patient/SignUp";
 
 const App: React.FC = () => {
   return (
-    <UserProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<PreLoginPage />} />
-
-          {/* Nurse Pages */}
-          <Route path="/nurse-login" element={<NurseLoginPage />} />
-          <Route path="/nurse-main" element={<NurseMainPage />} />
-
-          
-          {/* Patient Pages */}
-          <Route path="/patient-login" element={<PatientLoginPage />} />
-          <Route path="/patient-main" element={<PatientMainPage />} />
-          <Route path="/choose-patient-type" element={<ChoosePatientType />} />
-          <Route
-            path="/patient-chat"
-            element={
-              <FavoriteRequestsProvider>
-                <PatientChatPage />
-              </FavoriteRequestsProvider>
-            }
-          />
-          <Route
-            path="/patient-chat-categories"
-            element={
-              <FavoriteRequestsProvider>
-                <PatientChatCategories />
-              </FavoriteRequestsProvider>
-            }
-          />
-          <Route path="/custom-request" element={<CustomRequestPage />}/>
-          
-
-          <Route path='/test' element={<HospitalInfoPage />} />
-        </Routes>
-      </Router>
-    </UserProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PreLoginPage />} /> 
+        <Route path="/nurse-login" element={<NurseLoginPage />} /> 
+        <Route path="/nurse-main" element={<NurseMainPage />} />
+        <Route path="/nurse-schedule" element={<NurseSchedulePage />} />
+        <Route path="/patient-login" element={<PatientLoginPage />} />
+        <Route path="/patient-main" element={<PatientMainPage />} />
+        <Route path="/choose-patient-type" element={<ChoosePatientType />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/patient-schedular" element={<PatientSchedular />} />
+        <Route
+          path="/schedule-today"
+          element={
+            <ScheduleToday
+              onClose={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+              scheduleData={[]}
+              selectedTags={[]}
+              onTagClick={function (_tag: string): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          }
+        />
+        <Route
+          path="/patient-chat"
+          element={
+            <FavoriteRequestsProvider>
+              <PatientChatPage />
+            </FavoriteRequestsProvider>
+          }
+        />
+        <Route
+          path="/patient-chat-categories"
+          element={
+            <FavoriteRequestsProvider>
+              <PatientChatCategories />
+            </FavoriteRequestsProvider>
+          }
+        />
+        <Route path="/custom-request" element={<CustomRequestPage />} />
+      </Routes>
+    </Router>
   );
 };
 
